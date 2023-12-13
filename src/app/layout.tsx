1eed203c72853ad1aae01092cc478dfdecd0e7e1
Nikita Maslov <maslomeister@gmail.com>
2023-12-13 23:46:45 +0300

    feat: theme dark/light

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import cn from "@/utils/cn";
 import AppContext from "@/components/AppContextWrapper/AppContextWrapper";
+import { cookies } from "next/headers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,8 +13,13 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
+  // Get theme based on the cookie "theme".
+  const themeCookie = cookies().get("theme");
+  // If the cookie "theme" does not exist, set theme to the first index of Themes.
+  const currentTheme = themeCookie ? themeCookie.value : "light";
+
   return (
-    <html lang="ru" data-theme="dark">
+    <html lang="ru" data-theme={currentTheme}>
       <AppContext>
         <body className={cn(inter.className, "font-sans flex flex-col min-h-screen")}>{children}</body>
       </AppContext>
