a67bac03191d2762002e173150b3aaa7ef17be90
Nikita Maslov <maslomeister@gmail.com>
2023-12-14 21:57:34 +0300

    cleanup

--- src/components/Navbar/MobileMenu.tsx
+++ src/components/Navbar/MobileMenu.tsx
@@ -26,7 +26,7 @@ const MobileMenu = ({ dropdownDirection = "bottom" }: Props) => {
           className="inline-block w-8 h-8 stroke-current"
           // style={{ color }}
         >
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
+          <path stroke-linecap="round" stroke-linejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
         </svg>
       </label>
       <ul
