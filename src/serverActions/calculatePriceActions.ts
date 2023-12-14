38357be46bcb406ec0cefb25fff4676ec61f6866
Nikita Maslov <maslomeister@gmail.com>
2023-12-15 01:58:22 +0300

    feat: tinder

--- src/serverActions/calculatePriceActions.ts
+++ src/serverActions/calculatePriceActions.ts
@@ -212,13 +212,13 @@ export async function getPsEaPlayPrice(duration: string) {
   } catch (error) {
     if (error instanceof Error) {
       // Check if the error is an instance of the Error class
-      console.error("Error in getPsnAccountPrice:", error.message);
+      console.error("Error in getPsEaPlayPrice:", error.message);
       return { calculated: undefined, sale: undefined };
 
       // throw error; // Rethrow the error to propagate it to the calling code
     } else {
       // Handle other types of errors (if any)
-      console.error("Unknown error in getPsnAccountPrice:", error);
+      console.error("Unknown error in getPsEaPlayPrice:", error);
       return { calculated: undefined, sale: undefined };
 
       // throw new Error("An unknown error occurred."); // Rethrow a new error
@@ -265,13 +265,66 @@ export async function getSpotifyPrice(values: { subscriptionType: string; durati
   } catch (error) {
     if (error instanceof Error) {
       // Check if the error is an instance of the Error class
-      console.error("Error in getPsnAccountPrice:", error.message);
+      console.error("Error in getSpotifyPrice:", error.message);
       return { calculated: undefined, sale: undefined };
 
       // throw error; // Rethrow the error to propagate it to the calling code
     } else {
       // Handle other types of errors (if any)
-      console.error("Unknown error in getPsnAccountPrice:", error);
+      console.error("Unknown error in getSpotifyPrice:", error);
+      return { calculated: undefined, sale: undefined };
+
+      // throw new Error("An unknown error occurred."); // Rethrow a new error
+    }
+  }
+}
+
+export async function getTinderPrice(values: { subscriptionType: string; duration: string }) {
+  try {
+    const calcUrl = new URL(`https://api.digiseller.ru/api/products/price/calc`);
+
+    calcUrl.searchParams.append("product_id", process.env.DIGISELLER_TINDER_BASE_ID!);
+    calcUrl.searchParams.append("currency", "RBX");
+
+    if (values.subscriptionType !== "plus" || values.duration !== "1month") {
+      calcUrl.searchParams.append(
+        "options[]",
+        `${process.env.DIGISELLER_TINDER_OPTION_ID!}:${
+          process.env[`DIGISELLER_TINDER_${values.duration.toUpperCase()}_${values.subscriptionType.toUpperCase()}_VARIANT_ID`]
+        }`
+      );
+    }
+    const response = await fetch(calcUrl.toString());
+
+    if (!response.ok) {
+      // Handle non-successful HTTP response (e.g., 404, 500, etc.)
+      // throw new Error(`Failed to fetch data. Status: ${response.status}`);
+      return { calculated: undefined, sale: undefined };
+    }
+
+    const responseData = await response.json();
+
+    const data: {
+      price: number;
+      count: number;
+      amount: number;
+      currency: string;
+      commission: number;
+      free_pay: boolean | null;
+      sale_info: { common_base_price: number; sale_percent: number };
+    } = responseData.data;
+
+    return { calculated: Math.round(data.price * data.count), sale: data.amount };
+  } catch (error) {
+    if (error instanceof Error) {
+      // Check if the error is an instance of the Error class
+      console.error("Error in getTinderPrice:", error.message);
+      return { calculated: undefined, sale: undefined };
+
+      // throw error; // Rethrow the error to propagate it to the calling code
+    } else {
+      // Handle other types of errors (if any)
+      console.error("Unknown error in getTinderPrice:", error);
       return { calculated: undefined, sale: undefined };
 
       // throw new Error("An unknown error occurred."); // Rethrow a new error
--- src/serverActions/createPaymentUrls.ts
+++ src/serverActions/createPaymentUrls.ts
@@ -169,3 +169,40 @@ export async function getSpotifyPaymentLink(values: { subscriptionType: string; 
     data: { paymentUrl: paymentUrl.toString() },
   };
 }
+
+export async function getTinderPaymentLink(values: { subscriptionType: string; duration: string }) {
+  const paramsRes = await fetch("https://api.digiseller.ru/api/purchases/options", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      product_id: process.env.DIGISELLER_TINDER_BASE_ID,
+      options: [
+        {
+          id: process.env.DIGISELLER_TINDER_OPTION_ID,
+          value: {
+            id: process.env[`DIGISELLER_TINDER_${values.duration.toUpperCase()}_${values.subscriptionType.toUpperCase()}_VARIANT_ID`],
+          },
+        },
+      ],
+      lang: "ru-RU",
+      ip: "127.0.0.1",
+    }),
+  });
+  const { id_po } = await paramsRes.json();
+
+  const paymentUrl = new URL("https://oplata.info/asp2/pay_rk.asp");
+
+  paymentUrl.searchParams.append("id_d", process.env.DIGISELLER_TINDER_BASE_ID!);
+  paymentUrl.searchParams.append("id_po", id_po);
+  paymentUrl.searchParams.append("curr", "RBX");
+  paymentUrl.searchParams.append("lang", "ru-RU");
+  paymentUrl.searchParams.append("failpage", "https://mnepodpisku.ru/tinder");
+
+  return {
+    status: "success",
+    message: "success",
+    data: { paymentUrl: paymentUrl.toString() },
+  };
+}
