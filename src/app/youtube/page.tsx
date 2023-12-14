cb34321f915d61267de2da1827e9d8fa612d486e
Nikita Maslov <maslomeister@gmail.com>
2023-12-15 02:26:40 +0300

    feat: youtube

---
+++ src/app/youtube/page.tsx
@@ -0,0 +1,95 @@
+import React from "react";
+import HeroChoose from "@/components/HeroChoose/HeroChoose";
+import Faq from "@/components/Faq/Faq";
+import Question from "@/components/Faq/Question";
+import FormComponent from "./FormComponent";
+import RedirectingToPayment from "@/components/RedirectingToPayment/RedirectingToPayment";
+import { Metadata } from "next";
+import { isSearchParamValid } from "@/utils/utils";
+
+export const metadata: Metadata = {
+  title: "Купить Youtube premium 2023",
+  description:
+    "Быстро купить youtube premium на личный аккуант. Оплатить можно используя карты МИР, Qiwi, Яндекс Pay, PayPal, WebMoney и многие другие системы.",
+  openGraph: {
+    title: "Купить Youtube premium 2023 Россия",
+    description:
+      "Быстро купить youtube premium на личный аккуант. Оплатить можно используя карты МИР, Qiwi, Яндекс Pay, PayPal, WebMoney и многие другие системы.",
+  },
+  alternates: {
+    canonical: "/youtube",
+  },
+};
+
+type Props = {
+  searchParams: { [key: string]: string | string[] | undefined };
+};
+
+export default function PsEaPlay({ searchParams }: Props) {
+  return (
+    <div className="flex flex-col w-full max-w-[1240px] mx-auto mt-8 sm:mt-10 px-2 sm:px-4 mb-8">
+      <div className="flex flex-col min-[1240px]:flex-row items-center gap-4">
+        <h1 className="text-3xl lg:text-4xl font-bold text-center min-[1240px]:text-left" id="heading">
+          КУПИТЬ ПОДПИСКУ НА XBOX GAME PASS В РОССИИ
+        </h1>
+      </div>
+
+      <FormComponent receivedDuration={isSearchParamValid(searchParams["dur"])} />
+
+      <div className="mt-10">
+        <h2 className="text-xl lg:text-2xl font-bold" id="description">
+          Описание
+        </h2>
+        <p className="text-lg font-medium mt-4">
+          YouTube — видеохостинг, предоставляющий пользователям услуги хранения, доставки и показа видео.
+          <br />
+          <br />
+          Для того, чтобы оформить Youtube Premium в России необходимо предоставить логин и пароль для того, чтобы наши менеджеры смогли
+          активировать подписку на Ваш аккаунт. Подписка оформляется сроком на 1 или 12 месяцев.
+          <br />
+          <br />
+          <span className="font-bold">ВНИМАНИЕ!</span> Данная подписка является полностью индивидуальной. Мы не подключаем Вас к
+          семейной подписке, мы оплачиваем подписку на Ваш аккаунт.
+        </p>
+        <HeroChoose
+          firstText="Выберите нужный вам срок подписки"
+          secondText="Оплатите удобным для вас способом"
+          thirdText="Наслаждайтесь просмотром YouTube без рекламы"
+        />
+      </div>
+
+      <Faq>
+        <Question title="Входит ли Youtube Music в подписку?" open>
+          <p>Да, вы сможете использовать youtube music без ограничений при активной премиум подписке.</p>
+        </Question>
+        <Question title="Работает ли Youtube Premium в России?">
+          <p>Да, при активации подписки Вы сможете пользоваться всеми возможностями из России.</p>
+        </Question>
+        <Question title="Как производится активация Youtube Premium в России?">
+          <p>
+            Активация подписки происходит через наших менеджеров. Менеджер входит в Ваш аккаунт и оплачивает банковской картой подписку.
+          </p>
+        </Question>
+      </Faq>
+
+      <RedirectingToPayment />
+    </div>
+  );
+}
+
+// export const head: DocumentHead = {
+//   title: "МНЕПОДПИСКУ",
+//   meta: [
+//     {
+//       name: "description",
+//       content:
+//         "Сервис для приобретения подписок на различные онлайн сервисы. Принимаем к оплате карты МИР, Qiwi, Яндекс Pay, PayPal, WebMoney и многие другие системы.",
+//     },
+//   ],
+//   links: [
+//     {
+//       rel: "canonical",
+//       href: "https://mnepodpisku.ru/",
+//     },
+//   ],
+// };
--- src/serverActions/calculatePriceActions.ts
+++ src/serverActions/calculatePriceActions.ts
@@ -385,3 +385,57 @@ export async function getXboxPrice(values: { subscriptionType: string; duration:
     }
   }
 }
+
+export async function getYoutubePrice(duration: string) {
+  try {
+    const calcUrl = new URL(`https://api.digiseller.ru/api/products/price/calc`);
+
+    calcUrl.searchParams.append("product_id", process.env.DIGISELLER_YOUTUBE_PREMIUM_BASE_ID!);
+    calcUrl.searchParams.append("currency", "RBX");
+
+    if (duration !== "1month") {
+      calcUrl.searchParams.append(
+        "options[]",
+        `${process.env.DIGISELLER_YOUTUBE_PREMIUM_OPTION_ID!}:${process.env[
+          `DIGISELLER_YOUTUBE_PREMIUM_${duration.toUpperCase()}_VARIANT_ID`
+        ]!}`
+      );
+    }
+
+    const response = await fetch(calcUrl.toString());
+
+    if (!response.ok) {
+      // Handle non-successful HTTP response (e.g., 404, 500, etc.)
+      // throw new Error(`Failed to fetch data. Status: ${response.status}`);
+      return { calculated: undefined, sale: undefined };
+    }
+
+    const responseData = await response.json();
+
+    const data: {
+      price: number;
+      count: number;
+      amount: number;
+      currency: string;
+      commission: number;
+      free_pay: boolean | null;
+      sale_info: { common_base_price: number; sale_percent: number };
+    } = responseData.data;
+
+    return { calculated: Math.round(data.price * data.count), sale: data.amount };
+  } catch (error) {
+    if (error instanceof Error) {
+      // Check if the error is an instance of the Error class
+      console.error("Error in getTinderPrice:", error.message);
+      return { calculated: undefined, sale: undefined };
+
+      // throw error; // Rethrow the error to propagate it to the calling code
+    } else {
+      // Handle other types of errors (if any)
+      console.error("Unknown error in getTinderPrice:", error);
+      return { calculated: undefined, sale: undefined };
+
+      // throw new Error("An unknown error occurred."); // Rethrow a new error
+    }
+  }
+}
