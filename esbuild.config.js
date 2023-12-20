0a0a294a8017b70c113428e9619dc818738def93
Nikita Maslov <maslomeister@gmail.com>
2023-12-20 14:34:06 +0300

    refactor: try catch

--- esbuild.config.js
+++ esbuild.config.js
@@ -12,17 +12,22 @@
 const esbuild = require('esbuild');
 
 const build = async () => {
-	let result = await esbuild.build({
-		entryPoints: ['src/utils/generateOgImages/generateOgImages.tsx'],
-		platform: 'node',
-		bundle: true,
-		minify: true,
-		loader: { ".png": "dataurl", ".tsx": "tsx" },
-		external: ["@resvg/resvg-js"],
-		jsx: "automatic",
-		outfile: '/dist/out.js',
-	})
-	console.log(result)
+	try {
+		let result = await esbuild.build({
+			entryPoints: ['src/utils/generateOgImages/generateOgImages.tsx'],
+			platform: 'node',
+			bundle: true,
+			minify: true,
+			loader: { ".png": "dataurl", ".tsx": "tsx" },
+			external: ["@resvg/resvg-js"],
+			jsx: "automatic",
+			outfile: '/dist/out.js',
+		});
+		console.log(result);
+	} catch (err) {
+		console.log(err);
+	}
+
 }
 
 build();
