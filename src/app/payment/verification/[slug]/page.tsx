262402792f891dcc134fbd2cb2880dc70f10a379
Nikita Maslov <maslomeister@gmail.com>
2023-12-21 12:14:43 +0300

    feat: psn account

--- src/app/payment/verification/[slug]/page.tsx
+++ src/app/payment/verification/[slug]/page.tsx
@@ -6,6 +6,7 @@ import { ActivationTypes, verifyActivationType } from "@/utils/activationUtils";
 import PsPlusClient from "../PsPlusClient";
 import VerificationNotFound from "../VerificationNotFound";
 import VerificationSuccessClient from "../VerificationSuccessClient";
+import PsnAccountClient from "../PsnAccountClient";
 
 type Props = {
   params: { slug: string };
@@ -33,6 +34,8 @@ async function Page({ searchParams, params }: Props) {
   switch (activationTypes) {
     case "psn_balance":
       return <PsnBalanceClient code={uniquecode.code} />;
+    case "psn_account":
+      return <PsnAccountClient code={uniquecode.code} />;
     case "ps_plus":
       return <PsPlusClient code={uniquecode.code} />;
     case "ps_ea_play":
