d74bc7c6dbb80ed4b37eb85c6a706b93706a9d99
Nikita Maslov <maslomeister@gmail.com>
2023-12-22 21:16:23 +0300

    fix: not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,19 @@
+import Footer from "@/components/Footer/Footer";
+import Navbar from "@/components/Navbar/Navbar";
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <div className="h-[100svh]">
+      <Navbar isNotFound />
+      <main className="h-[calc(100%-260px)] flex flex-col justify-center items-center">
+        <h1 className="text-3xl font-bold">404</h1>
+        <h2 className="text-xl font-semibold">Страница не найдена</h2>
+        <Link className="btn btn-secondary text-white mt-4" href="/">
+          На главную
+        </Link>
+      </main>
+      <Footer />
+    </div>
+  );
+}
