676c433742826ce4b0dfaa5d7b1804336994ba13
Nikita Maslov <maslomeister@gmail.com>
2023-12-24 00:19:00 +0300

    feat: dynamic reviews

---
+++ src/app/api/reviews/get_reviews/route.ts
@@ -0,0 +1,141 @@
+type VkResponse = {
+  items: {
+    date: number;
+    from_id: number;
+    id: number;
+    text: string;
+  }[];
+  profiles: {
+    id: number;
+    sex: number;
+    screen_name: string;
+    photo_50: string;
+    photo_100: string;
+    online_info: {
+      visible: boolean;
+      last_seen: number;
+      is_online: boolean;
+      app_id: number;
+      is_mobile: boolean;
+    };
+    online: number;
+    first_name: string;
+    last_name: string;
+    can_access_closed: boolean;
+    is_closed: boolean;
+  }[];
+};
+
+type WbResponse = {
+  id: string;
+  text: string;
+  productValuation: number;
+  createdDate: string;
+  answer: string;
+  state: string;
+  productDetails: {
+    imtId: number;
+    nmId: number;
+    productName: string;
+    supplierArticle: string;
+    supplierName: string;
+    brandName: string;
+    size: string;
+  };
+  video: string;
+  wasViewed: boolean;
+  photoLinks: string;
+  userName: string;
+  matchingSize: string;
+  isAbleSupplierFeedbackValuation: boolean;
+  supplierFeedbackValuation: number;
+  isAbleSupplierProductValuation: boolean;
+  supplierProductValuation: number;
+  isAbleReturnProductOrders: boolean;
+  returnProductOrdersDate: boolean;
+  bables: boolean;
+};
+
+function shuffleArray<T>(array: T[]): T[] {
+  let currentIndex = array.length;
+  let temporaryValue: T;
+  let randomIndex: number;
+
+  // While there remain elements to shuffle...
+  while (currentIndex !== 0) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+// Combine two arrays and shuffle the items
+function combineAndShuffleArrays(array1: any[], array2: any[]): any[] {
+  // Combine arrays
+  const combinedArray = array1.concat(array2);
+
+  // Shuffle the combined array
+  const shuffledArray = shuffleArray(combinedArray);
+
+  return shuffledArray;
+}
+
+export async function GET() {
+  try {
+    const commentsVk = await fetch(
+      "https://api.vk.com/method/board.getComments?group_id=221413404&topic_id=49184185&count=100&sort=desc&v=5.199&extended=true",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${process.env.VK_SERVICE_KEY}`,
+        },
+        next: { revalidate: 60 * 60 * 24 * 2 },
+      }
+    );
+
+    const result: VkResponse = (await commentsVk.json()).response;
+
+    const comments = result.items.filter((item) => item.from_id !== -221413404);
+    const reviewsVk = result.profiles.map((item) => {
+      const comment = comments.find((item_comment) => item_comment.from_id === item.id);
+      return { review: comment?.text, name: item.first_name, platform: "VK" };
+    });
+
+    const wbRequestReviews = await fetch(
+      "https://feedbacks-api.wildberries.ru/api/v1/feedbacks?isAnswered=true&take=100&skip=0&order=dateDesc",
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${process.env.WB_GET_REVIEWS_TOKEN}`,
+        },
+        next: { revalidate: 60 * 60 * 24 * 2 },
+      }
+    );
+
+    const wbRes: WbResponse[] = (await wbRequestReviews.json()).data.feedbacks;
+
+    const filteredReviewsWb = wbRes.filter((item) => item.text);
+    const finalReviewsWb = filteredReviewsWb.map((item) => {
+      return {
+        review: item.text,
+        name: item.userName !== "" ? item.userName : "Покупатель",
+        platform: "WB",
+        rating: item.productValuation,
+      };
+    });
+
+    return Response.json(combineAndShuffleArrays(reviewsVk, finalReviewsWb));
+  } catch (err) {
+    console.log("error in api/reviews/get_reviews/", err);
+    return Response;
+  }
+}
