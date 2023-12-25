87da84d3e73cd7504c9e0d5342c04cb11718d626
Nikita Maslov <maslomeister@gmail.com>
2023-12-25 04:15:31 +0300

    refactor: updated text

--- src/components/WeAreOnBrakeBanner/WeAreOnBrakerBanner.tsx
+++ src/components/WeAreOnBrakeBanner/WeAreOnBrakerBanner.tsx
@@ -1,16 +1,16 @@
 function WeAreOnBrakerBanner() {
   return (
-    <div className="w-full bg-base-content py-1">
+    <div className="w-full bg-base-300 py-1">
       <a
-        className="flex text-lg gap-1 md:gap-2 w-full max-w-[1240px] mx-auto justify-between text-base-100 font-medium flex-col md:flex-row"
+        className="flex text-lg p-1 gap-1 md:gap-2 w-full max-w-[1240px] mx-auto justify-between font-medium flex-col md:flex-row"
         target="_blank"
         rel="noopener noreferrer"
       >
-        <div className="flex gap-2 justify-between md:justify-start">
+        <div className="flex gap-2 justify-between md:justify-start font-bold">
           <p>График работы на</p>
           <p>26.12.23-06.01.24</p>
         </div>
-        <p className=" text-end">Узнать подробности</p>
+        <a className="text-center md:text-end underline decoration-warning">Узнать подробности</a>
       </a>
     </div>
   );
