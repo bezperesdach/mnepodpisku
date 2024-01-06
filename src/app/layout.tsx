a1d1cfaa96ad3a7be2bf3d9452e4fe671a7dd5b1
Nikita Maslov <maslomeister@gmail.com>
2024-01-06 17:43:03 +0300

    refactor: remove banner

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -54,7 +54,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
               __html: setInitialTheme,
             }}
           />
-          <WeAreOnBrakerBanner />
+          {/* <WeAreOnBrakerBanner /> */}
           {children}
         </body>
       </AppContextProvider>
