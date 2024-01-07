838e7d00c77c6ce074f5f8c9d68dfb7620e1ed92
Nikita Maslov <maslomeister@gmail.com>
2024-01-07 19:04:32 +0300

    feat: recalculate function

--- src/hooks/useIsOverflow.tsx
+++ src/hooks/useIsOverflow.tsx
@@ -5,24 +5,25 @@ interface UseIsOverflowProps {
   callback?: ((isOverflow: boolean) => void) | undefined;
 }
 
-export const useIsOverflow = (ref: MutableRefObject<any>, { callback }: UseIsOverflowProps = {}): boolean | undefined => {
+export const useIsOverflow = (
+  ref: MutableRefObject<any>,
+  { callback }: UseIsOverflowProps = {}
+): { isOverflow: boolean | undefined; recalculateOverflow: () => void } => {
   const [isOverflow, setIsOverflow] = useState<boolean | undefined>(undefined);
 
-  useLayoutEffect(() => {
-    const { current } = ref;
-
-    const trigger = () => {
-      const hasOverflow = current.scrollHeight - 8 > current.clientHeight;
-
+  const recalculateOverflow = () => {
+    if (ref.current) {
+      const hasOverflow = ref.current.scrollHeight - 8 > ref.current.clientHeight;
       setIsOverflow(hasOverflow);
 
       if (callback) callback(hasOverflow);
-    };
-
-    if (current) {
-      trigger();
     }
+  };
+
+  useLayoutEffect(() => {
+    recalculateOverflow();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [callback, ref]);
 
-  return isOverflow;
+  return { isOverflow, recalculateOverflow };
 };
