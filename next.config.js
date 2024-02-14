f6b2b30afc25f42643dfdf5e10200aa9e4c8d801
maslomeister <maslomeister@gmail.com>
2024-02-14 11:40:06 +0300

    refactor: redirect /activate/wb to /wb

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+	async redirects() {
+		return [
+			{
+				source: '/activate/wb',
+				destination: '/wb',
+				permanent: true,
+			},
+		]
+	},
 	experimental: {
 		serverComponentsExternalPackages: ["@resvg/resvg-js"],
 	},
