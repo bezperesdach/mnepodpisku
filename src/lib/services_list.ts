6364e3195ca0084f1316889596bbc0467cde2acb
maslomeister <maslomeister@gmail.com>
2024-03-14 10:55:50 +0300

    list of services

---
+++ src/lib/services_list.ts
@@ -0,0 +1,13 @@
+const spotify = {
+	name: "Spotify",
+	value: "spotify",
+	keywords: ["Спотифай","Премиум","Подписка спотифай","Premium"]
+}
+
+const playstation = {
+	name: "PlayStation",
+	value: "playstation",
+	keywords: ["psn","плейстейшн","псн","ps4","ps5"]
+}
+
+export const services = [spotify, playstation]
