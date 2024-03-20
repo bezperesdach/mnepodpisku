dfd09b12c6102d3c94c1cb48059a360f8d79851e
maslomeister <maslomeister@gmail.com>
2024-03-20 10:38:39 +0300

    new style

--- src/components/Faq/Question.tsx
+++ src/components/Faq/Question.tsx
@@ -1,35 +1,20 @@
 "use client";
 
+import { AccordionContent, AccordionItem, AccordionTrigger } from "../ui/accordion";
+
 type Props = {
   title: string;
   open?: boolean;
   children: React.ReactNode;
-  id?: string;
+  id: string;
 };
 
 const Question = ({ id, title, open, children }: Props) => {
   return (
-    <details className="group bg-base-200 rounded-2xl mt-6" open={open} id={id}>
-      <summary className="flex justify-between items-center text-xl font-medium cursor-pointer list-none px-4 py-4">
-        <span className="pr-4">{title}</span>
-        <span className="transition group-open:rotate-180">
-          <svg
-            fill="none"
-            height="24"
-            shapeRendering="geometricPrecision"
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="1.5"
-            viewBox="0 0 24 24"
-            width="24"
-          >
-            <path d="M6 9l6 6 6-6"></path>
-          </svg>
-        </span>
-      </summary>
-      <div className="px-4 pb-4">{children}</div>
-    </details>
+    <AccordionItem value={id}>
+      <AccordionTrigger>{title}</AccordionTrigger>
+      <AccordionContent>{children}</AccordionContent>
+    </AccordionItem>
   );
 };
 
