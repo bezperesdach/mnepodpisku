220903875847c0fe74332665cb44ba7a0fb774c5
maslomeister <maslomeister@gmail.com>
2024-03-23 10:01:22 +0300

    redisgn netflix

---
+++ src/app/(products)/netflix/[[...slug]]/page.tsx
@@ -0,0 +1,95 @@
+import React from "react";
+import HeroChoose from "@/components/HeroChoose/HeroChoose";
+import Faq from "@/components/Faq/Faq";
+import Question from "@/components/Faq/Question";
+import FormComponent from "./FormComponent";
+import RedirectingToPayment from "@/components/RedirectingToPayment/RedirectingToPayment";
+import { Metadata } from "next";
+import { isSearchParamValid } from "@/utils/utils";
+import Reviews from "@/components/Reviews/review";
+import { Breadcrumbs } from "@/components/breadcrumbs";
+import { BreadcrumbItem } from "@/components/ui/breadcrumb";
+import { ProductHero } from "@/components/product-hero";
+import { FlameIcon } from "@primer/octicons-react";
+
+export const metadata: Metadata = {
+  title: "Купить подписку Netflix",
+  description:
+    "Быстрая онлайн покупка подписки на Netflix. Оплатить можно используя карты МИР, Qiwi, Яндекс Pay, PayPal, WebMoney и многие другие системы.",
+  openGraph: {
+    title: "Купить подписку Netflix 2024 Промокод",
+    description:
+      "Быстрая онлайн покупка подписки на Netflix. Оплатить можно используя карты МИР, Qiwi, Яндекс Pay, PayPal, WebMoney и многие другие системы.",
+    url: "/netflix",
+    images: ["/og_images_catalogue/netflix.png"],
+    type: "website",
+  },
+  alternates: {
+    canonical: "/netflix",
+  },
+};
+
+const checkParams = (slug: string | string[] | undefined) => {
+  if (slug && Array.isArray(slug) && slug.length > 0) {
+    if (slug[0]) {
+      if (["basic", "standard", "premium"].includes(slug[0])) {
+        if (slug[1]) {
+          const num = parseInt(slug[1], 10);
+          if (!isNaN(num) && Number.isInteger(num) && (num === 1 || num === 3 || num === 12)) {
+            return { type: slug[0], duration: slug[1] };
+          } else {
+            return { type: slug[0], duration: "1" };
+          }
+        }
+        return { type: slug[0], duration: "1" };
+      }
+    }
+  }
+
+  return undefined;
+};
+
+export default function Netflix({ params }: { params: { slug: string | string[] | undefined } }) {
+  return (
+    <div className="flex flex-col">
+      <Breadcrumbs>
+        <BreadcrumbItem>Netflix</BreadcrumbItem>
+      </Breadcrumbs>
+
+      <ProductHero
+        icon={{ src: "/catalogue_icons/netflix.jpg", alt: "Netflix баннер" }}
+        title="Купить подписку на Netflix в России"
+        description="Netflix — это стриминговый сервис, который предлагает широкий выбор отмеченных наградами телешоу, фильмов, аниме, документальных фильмов и многого другого на тысячах подключенных к Интернету устройств."
+        tags={[
+          {
+            icon: <FlameIcon className="text-[#f95721]" />,
+            text: "ВЫГОДНО",
+          },
+        ]}
+      />
+
+      <FormComponent subscriptionType={checkParams(params.slug)} />
+
+      <Reviews />
+
+      <Faq>
+        <Question title="Как это работает?">
+          <p>
+            После успешной оплаты вам на почту придет письмо с ссылкой на активацию
+            <br />
+            <br />
+            На странице активации вам необходимо будет указать данные своего аккаунта Netflix, после чего отправить нам заявку на
+            активацию
+            <br />
+            <br />
+            Наш менеджер получит вашу заявку и приобретет подписку на ваш аккаунт{" "}
+          </p>
+        </Question>
+
+        <Question title="Как быстро я получу подписку Netflix?">
+          <p>После отправки заявки среднее время выполнения заказа 5-30 минут в рабочие часы</p>
+        </Question>
+      </Faq>
+    </div>
+  );
+}
