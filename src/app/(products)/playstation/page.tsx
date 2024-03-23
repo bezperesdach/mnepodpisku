04972fa4eb6cb63dd9bd8ebdcbb7a8d0daeb77b9
maslomeister <maslomeister@gmail.com>
2024-03-23 05:20:55 +0300

    cleanup and new component

--- src/app/(products)/playstation/page.tsx
+++ src/app/(products)/playstation/page.tsx
@@ -32,33 +32,12 @@ import { BreadcrumbItem } from "@/components/ui/breadcrumb";
 import Image from "next/image";
 import { Skeleton } from "@/components/ui/skeleton";
 import { FlameIcon, StarFillIcon } from "@primer/octicons-react";
-import { Service } from "@/app/(main)/all-services";
+import { AlsoRecommendToBuy } from "@/components/also-recommend-to-buy";
 // import UnclosableDrawerModalHybridPaymentRedirect from "@/components/ui/unclosable-drawer-modal-hybrid-payment-redirect";
 
 type Props = {
   searchParams: { [key: string]: string | string[] | undefined };
 };
-
-const services: Service[] = [
-  {
-    name: "PlayStation Аккаунт",
-    value: "playstation_account",
-    imageSrc: "/catalogue_icons/playstation_account.jpg",
-    alt: "PlayStation Аккаунт баннер",
-  },
-  {
-    name: "PlayStation Plus",
-    value: "playstation_plus",
-    imageSrc: "/catalogue_icons/playstation_plus.jpg",
-    alt: "PlayStation Plus баннер",
-  },
-  {
-    name: "PlayStation Ea Play",
-    value: "ps_ea_play",
-    imageSrc: "/catalogue_icons/playstation_ea_play.jpg",
-    alt: "PlayStation Ea Play баннер",
-  },
-];
 
 export const metadata: Metadata = {
   title: "Пополнение PS Store(PSN) турция с карты РФ от 100 TL!",
@@ -147,30 +126,34 @@ export default function PlaystationV2({ searchParams }: Props) {
 
       <FormComponent receivedAmount={isAmountValid(searchParams["amount"])} card={isSearchParamValid(searchParams["card"])} ip={ip} />
 
-      <div className="w-full flex justify-center items-center mt-16">
-        <div className="w-full flex flex-col max-w-screen-lg px-2">
-          <div className="w-full flex justify-between">
-            <h2 className="text-3xl font-semibold tracking-tight">Также рекомендуем</h2>
-          </div>
-          <div className="grid md:flex gap-x-4 gap-y-6 md:gap-4 w-full grid-cols-2 md:grid-cols-3 mt-6">
-            {services.map((item) => (
-              <div key={item.value} className="flex-1 flex-shrink-0 rounded-md ">
-                <div className="min-h-[120px] xs:min-h-[160px] sm:min-h-[170px] lg:min-h-[180px] relative rounded-3xl overflow-hidden">
-                  <Image
-                    className="hover:scale-125 transit`ion-all z-10"
-                    src={item.imageSrc}
-                    alt={item.alt}
-                    style={{ objectFit: "cover" }}
-                    fill
-                  />
-                  <Skeleton className="absolute w-full h-full rounded-3xl" />
-                </div>
-                <p className="mt-4 lg:text-lg text-center font-medium">{item.name}</p>
-              </div>
-            ))}
-          </div>
-        </div>
-      </div>
+      <AlsoRecommendToBuy
+        services={[
+          {
+            name: "PlayStation Аккаунт",
+            value: "playstation_account",
+            imageSrc: "/catalogue_icons/playstation_account.jpg",
+            alt: "PlayStation Аккаунт баннер",
+          },
+          {
+            name: "PlayStation Plus",
+            value: "playstation_plus",
+            imageSrc: "/catalogue_icons/playstation_plus.jpg",
+            alt: "PlayStation Plus баннер",
+          },
+          {
+            name: "PlayStation Ea Play",
+            value: "ps_ea_play",
+            imageSrc: "/catalogue_icons/playstation_ea_play.jpg",
+            alt: "PlayStation Ea Play баннер",
+          },
+          {
+            name: "",
+            value: "",
+            imageSrc: "",
+            alt: "",
+          },
+        ]}
+      />
 
       <Reviews />
 
@@ -206,114 +189,7 @@ export default function PlaystationV2({ searchParams }: Props) {
         </Question>
       </Faq>
 
-      {/* <Description />
-
-      <Reviews />
-
-      <Faq>
-        <Question title="Пополнение кошелька PlayStation в Турции: Как происходит пополнение?" >
-          <p>
-            После оплаты вы будете перенаправлены на страницу активации
-            <br />
-            <br />
-            На этой странице вам необходимо будет указать данные вашего аккаунта и резервный код, после этого вы вышлите нам эти данные
-            по удобному вам каналу связи
-            <br />
-            <br /> После получения вашего запроса на активацию наш менеджер выполнит вход и пополнит ваш баланс на необходимую вам сумму
-            <br />
-            <br />
-            <span className="font-semibold">Важно!</span> Для успешного пополнения необходимо иметь аккаунт PlayStation Network с
-            регионом Турция. В случае, если у вас нет турецкого аккаунта, вы можете{" "}
-            <a className="link text-secondary" href="/playstation_account" target="_blank" rel="noopener noreferrer">
-              приобрести его создание у нас
-            </a>{" "}
-            по скромной цене.
-          </p>
-        </Question>
-
-        <Question title="Как быстро происходит пополнение?">
-          <p>После выполнения всех инструкций среднее время обработки заказа 5-30 минут при обращении в рабочие часы.</p>
-        </Question>
-
-        <Question title="Почему нужен именно турецкий аккаунт PSN?">
-          <p>
-            Так как пополнение вашего аккаунта PlayStation происходит с помощью турецкой банковской карты, аккаунт должен быть
-            зарегистрирован в турецком регионе.
-            <br />
-            <br />
-            Такой аккаунт вы можете создать сами по инструкции "Как создать турецкий аккаунт PSN" ниже
-            <br />
-            <br />
-            Или можете воспользоваться нашей услугой по созданию аккаунта нажав{" "}
-            <a className="link text-secondary" href="/playstation_account" target="_blank" rel="noopener noreferrer">
-              сюда
-            </a>
-          </p>
-        </Question>
-
-        <Question title="Почему именно Турция?">
-          <p>
-            Одной из основных причин выбора Турции является значительная экономия на покупке игр и подписок в сравнении с Россией. Игры
-            и подписки в турецком регионе PlayStation Store доступны по гораздо более низким ценам. <br />
-            <br />
-            Цены на игры и подписки в Турции существенно отличаются от российских цен, и позволяют сэкономить значительную сумму денег.{" "}
-            <br />
-            <br />
-            Таким образом, пополнение кошелька в Турции позволяет игрокам приобретать игры и подписки по более выгодной цене, и получать
-            больше контента за свои средства.
-          </p>
-        </Question>
-
-        <Question title="Что будет если вам не удастся пополнить мой аккаунт?">
-          <p>Мы можем сделать возврат средств согласно оферте нашего сайта</p>
-        </Question>
-        <Question title="Как приобрести PS PLUS на свой аккаунт PlayStation?">
-          <p>
-            В самом верху страницы или в самом низу нажмите на "Каталог"
-            <br />
-            <br />
-            Выберите в каталоге PS PLUS и приобретите
-          </p>
-        </Question>
-        <Question title="Как приобрести EA PLAY на свой аккаунт PlayStation?">
-          <p>
-            В самом верху страницы или в самом низу нажмите на "Каталог"
-            <br />
-            <br />
-            Выберите в каталоге PS EA PLAY и приобретите
-          </p>
-        </Question>
-        <Question title="Почему возникают проблемы с новыми аккаунтами?" >
-          <p>
-            Начиная от 01.11.23 Sony изменили что-то в своей системе, если раньше после создания аккаунт можно было пополнять без особых
-            проблем спустя неделю после создания, то теперь после создания аккаунт стабильно удается пополнять только спустя месяц.
-            <br />
-            <br />
-            Сразу после создания аккаунт можно либо пополнить на 200 ЛИР(Максимальная разовая транзакция пополнения), либо приобрести
-            предметы из корзины на сумму до 4800 ЛИР включительно (подписки в корзину не добавляются). После чего на аккаунт со стороны
-            Sony накладывается временный &quot;блок&quot; и нужно будет подождать примерно месяц, перед тем как на аккаунт можно будет
-            купить что-либо еще или произвести пополнение.
-          </p>
-        </Question>
-        <Question title="Что значит 'одноразовая карта' в форме заказа?" >
-          <p>
-            Данная опция позволит вам получить от нас данные одноразовой турецкой карты с номиналом на сумму вашего пополнения.
-            <br />
-            <br />
-            С помощью данной карты вы сможете самостоятельно приобрести из корзины игры/dlc/донаты. Подписки с помощью данной карты
-            приобрести нельзя.
-            <br />
-            <br />
-            Карта будет действовать ровно 1 неделю со дня покупки. Карта действует 1 раз. При попытки списания суммы выше номинала карта
-            - она автоматически заблокируется.
-            <br />
-            <br />
-            Данный способ пополнения подойдет людям которые не хотят передавать данные своего аккаунта
-          </p>
-        </Question>
-      </Faq>
-
-      <InstructionsSection>
+      {/* <InstructionsSection>
         <Instruction name="Как включить 2FA на аккаунте PSN?" file="kak_vkluchit_2fa_na_akaunte_psn" />
         <Instruction name="Где найти резервные коды аккаунта PSN?" file="gde_posmotret_rezervnyi_kod" />
         <Instruction name="Как изменить почту на PSN аккаунте" file="psn/how-to-change-email-psn.pdf" />
@@ -321,25 +197,6 @@ export default function PlaystationV2({ searchParams }: Props) {
         <Instruction name="Как добавить нового пользователя на PS4?" file="kak_dobavit_novogo_polzovatelya_na_PS4" />
         <Instruction name="Как добавить нового пользователя на PS5?" file="kak_dobavit_novogo_polzovatelya_na_PS5" />
       </InstructionsSection> */}
-
-      {/* <UnclosableDrawerModalHybridPaymentRedirect /> */}
     </div>
   );
 }
-
-// export const head: DocumentHead = {
-//   title: "МНЕПОДПИСКУ",
-//   meta: [
-//     {
-//       name: "description",
-//       content:
-//         "Сервис для приобретения подписок на различные онлайн сервисы. Принимаем к оплате карты МИР, Qiwi, Яндекс Pay, PayPal, WebMoney и многие другие системы.",
-//     },
-//   ],
-//   links: [
-//     {
-//       rel: "canonical",
-//       href: "https://mnepodpisku.ru/",
-//     },
-//   ],
-// };
