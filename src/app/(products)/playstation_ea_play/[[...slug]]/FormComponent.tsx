f223b32eda1cddac4b492270728698bffbdd0525
maslomeister <maslomeister@gmail.com>
2024-03-23 09:25:49 +0300

    cleanup

--- src/app/(products)/playstation_ea_play/[[...slug]]/FormComponent.tsx
+++ src/app/(products)/playstation_ea_play/[[...slug]]/FormComponent.tsx
@@ -1,14 +1,12 @@
 "use client";
 
 import { AppContext } from "@/components/AppContextWrapper/AppContextWrapper";
-import PaymentOptions from "@/components/PaymentOptions/PaymentOptions";
-import PriceComponent from "@/components/PriceComponent.tsx/PriceComponent";
 import ToggleSelect from "@/components/ToggleSelect/ToggleSelect";
 import { getPsEaPlayPrice } from "@/serverActions/calculatePriceActions";
 import { getPsEaPlayPaymentLink } from "@/serverActions/createPaymentUrls";
 import { cn } from "@/lib/utils";
 import { ym } from "@/utils/ym";
-import { LockIcon, SyncIcon } from "@primer/octicons-react";
+import { SyncIcon } from "@primer/octicons-react";
 import { useFormik } from "formik";
 import { usePathname, useRouter } from "next/navigation";
 import { useContext, useEffect, useState } from "react";
@@ -136,60 +134,6 @@ export default function FormComponent({ receivedDuration }: Props) {
           ym("reachGoal", "formaoplatit");
         }}
       />
-    </>
-  );
-
-  return (
-    <>
-      <form className="flex flex-col md:flex-row mt-4 md:mt-14 gap-4 sm:gap-8 md:gap-16" onSubmit={formik.handleSubmit}>
-        <div className="flex flex-col gap-1 lg:gap-6 w-full md:w-1/2">
-          <div className="flex flex-col gap-2 md:mb-4">
-            <div className="flex flex-col gap-2">
-              <p className="label font-medium">Выберите срок подписки:</p>
-              <ToggleSelect
-                options={[
-                  { name: "1 мес.", value: "1month" },
-                  { name: "12 мес.", value: "12month" },
-                ]}
-                value={formik.values.duration}
-                onSelect={(value) => formik.setFieldValue("duration", value)}
-              />
-            </div>
-            <div className="w-full flex-col gap-1 items-center hidden mt-4 md:flex ">
-              <button
-                type="submit"
-                className={cn("btn btn-secondary w-full text-white items-center", { "btn-disabled": formik.isSubmitting })}
-              >
-                {formik.isSubmitting ? (
-                  <span className="loading loading-spinner loading-xl flex-shrink-0" />
-                ) : (
-                  <LockIcon className="text-white text-xl" />
-                )}
-                Оплатить
-              </button>
-              <p className="text-center text-gray-500">После нажатия вы будете перенаправлены на страницу оплаты </p>
-            </div>
-          </div>
-        </div>
-        <div className="flex flex-col w-full md:w-1/2">
-          <PaymentOptions />
-          <PriceComponent loading={loading} value={calculatedAmount} sale={value} />
-          <div className="w-full flex-col gap-1 items-center md:hidden mt-4">
-            <button
-              type="submit"
-              className={cn("btn btn-secondary w-full text-white items-center", { "btn-disabled": formik.isSubmitting })}
-            >
-              {formik.isSubmitting ? (
-                <span className="loading loading-spinner loading-xl flex-shrink-0" />
-              ) : (
-                <LockIcon className="text-white text-xl" />
-              )}
-              Оплатить
-            </button>
-            <p className="text-center text-gray-500">После нажатия вы будете перенаправлены на страницу оплаты </p>
-          </div>
-        </div>
-      </form>
     </>
   );
 }
