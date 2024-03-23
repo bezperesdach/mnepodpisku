2d8c52e4dcab80c092a9edb5eb8829614e993cef
maslomeister <maslomeister@gmail.com>
2024-03-23 08:56:06 +0300

    playstation ea play redisgn

--- src/app/(products)/playstation_ea_play/[[...slug]]/FormComponent.tsx
+++ src/app/(products)/playstation_ea_play/[[...slug]]/FormComponent.tsx
@@ -8,10 +8,12 @@ import { getPsEaPlayPrice } from "@/serverActions/calculatePriceActions";
 import { getPsEaPlayPaymentLink } from "@/serverActions/createPaymentUrls";
 import { cn } from "@/lib/utils";
 import { ym } from "@/utils/ym";
-import { LockIcon } from "@primer/octicons-react";
+import { LockIcon, SyncIcon } from "@primer/octicons-react";
 import { useFormik } from "formik";
 import { usePathname, useRouter } from "next/navigation";
 import { useContext, useEffect, useState } from "react";
+import { Button } from "@/components/ui/button";
+import RedirectingToPayment from "@/components/RedirectingToPayment/RedirectingToPayment";
 
 type Props = {
   receivedDuration?: string;
@@ -28,11 +30,9 @@ export default function FormComponent({ receivedDuration }: Props) {
   const [loading, setLoading] = useState(true);
 
   const formik = useFormik({
-    initialValues: { duration: receivedDuration ?? "1month" },
+    initialValues: { duration: receivedDuration ?? "1" },
     onSubmit: async (values) => {
       formik.setSubmitting(true);
-      ym("reachGoal", "psEaPlayRequest");
-      ym("reachGoal", "formaoplatit");
 
       const res = await getPsEaPlayPaymentLink(values.duration);
 
@@ -44,11 +44,11 @@ export default function FormComponent({ receivedDuration }: Props) {
 
   useEffect(() => {
     const updatePrices = async (duration: string) => {
-      const current = new URLSearchParams();
-      current.set("dur", duration);
-      const search = current.toString();
-      const query = search ? `?${search}` : "";
-      router.replace(`${pathname}${query}`, { scroll: false });
+      if (duration === "1") {
+        history.pushState({ duration }, "", `/playstation_ea_play/`);
+      } else {
+        history.pushState({ duration }, "", `/playstation_ea_play/${duration}`);
+      }
 
       const updatedPrices = await getPsEaPlayPrice(duration);
       setCalculatedAmount(updatedPrices.calculated);
@@ -62,6 +62,82 @@ export default function FormComponent({ receivedDuration }: Props) {
 
     updatePrices(duration);
   }, [formik.values.duration, router, pathname]);
+
+  return (
+    <>
+      <form onSubmit={formik.handleSubmit}>
+        <div className="w-full flex justify-center items-center mt-6">
+          <div className="w-full flex flex-col gap-2 max-w-screen-lg mx-2 p-6 rounded-3xl bg-[#0c1430]">
+            <p className="text-2xl md:text-3xl font-semibold tracking-tight">Выберите срок подписки</p>
+
+            <ToggleSelect
+              options={[
+                { name: "1 мес.", value: "1" },
+                { name: "12 мес.", value: "12" },
+              ]}
+              value={formik.values.duration}
+              onSelect={(value) => formik.setFieldValue("duration", value)}
+            />
+          </div>
+        </div>
+        <div className="w-full flex justify-center items-center mt-6">
+          <div className="w-full flex flex-col gap-2 max-w-screen-lg mx-2 p-6 rounded-3xl bg-[#0c1430]">
+            <p className="text-2xl md:text-3xl font-semibold tracking-tight">К оплате</p>
+
+            <p className="tracking-tight text-muted-foreground">
+              После оплаты на указанную при оплате почту будет выслана инструкция по активации
+            </p>
+            <p className="tracking-tight text-muted-foreground">
+              С помощью этой инструкции вы сможете безопасно передать нам данные своего аккаунта на который будет приобретена подписка
+            </p>
+
+            <div className="flex flex-col gap-2 w-full justify-between mt-6">
+              <div className="flex justify-between w-full pb-1 border-b-[1px]">
+                <p className="text-lg text-muted-foreground">Заплатите</p>
+                <div className="flex gap-1 items-center text-lg text-muted-foreground">
+                  {loading && <SyncIcon className="animate-spin" />}
+                  {!loading && value && (
+                    <>
+                      {calculatedAmount && (
+                        <p className="relative mr-2 after:w-[110%] after:-rotate-[15deg] after:absolute after:-left-[5%] after:top-1/2 after:h-[0.15em] after:bg-[#e85426]/50">
+                          {calculatedAmount}₽
+                        </p>
+                      )}
+                      <p>{value}₽</p>
+                    </>
+                  )}
+                </div>
+              </div>
+            </div>
+            <Button
+              className={cn("sticky bottom-0 mt-6 text-lg h-12", {
+                "bg-accent/50 text-muted-foreground": !value,
+                "pointer-events-none": formik.isSubmitting,
+              })}
+            >
+              {!loading && !formik.isSubmitting ? (
+                <p>ОПЛАТИТЬ {value && <span className="text-[#fee525] text-xl font-bold">{value}₽</span>}</p>
+              ) : (
+                <SyncIcon className="animate-spin" />
+              )}
+            </Button>
+            <p className=" text-muted-foreground text-sm text-center">
+              Нажимая на кнопку «Оплатить», вы соглашаетесь с{" "}
+              <a className="text-primary hover:underline" href="/oferta.pdf" target="_blank" rel="noopener noreferrer">
+                договором оферты
+              </a>
+            </p>
+          </div>
+        </div>
+      </form>
+      <RedirectingToPayment
+        onRedirect={() => {
+          ym("reachGoal", "psEaPlayRequest");
+          ym("reachGoal", "formaoplatit");
+        }}
+      />
+    </>
+  );
 
   return (
     <>
---
+++ src/app/(products)/playstation_ea_play/[[...slug]]/page.tsx
@@ -0,0 +1,126 @@
+import React from "react";
+import Faq from "@/components/Faq/Faq";
+import Question from "@/components/Faq/Question";
+import FormComponent from "./FormComponent";
+import { Metadata } from "next";
+import { isSearchParamValid } from "@/utils/utils";
+import Reviews from "@/components/Reviews/review";
+import { Breadcrumbs } from "@/components/breadcrumbs";
+import { BreadcrumbItem } from "@/components/ui/breadcrumb";
+import { ProductHero } from "@/components/product-hero";
+import Image from "next/image";
+import { FlameIcon, StarFillIcon } from "@primer/octicons-react";
+import { AlsoRecommendToBuy } from "@/components/also-recommend-to-buy";
+
+export const metadata: Metadata = {
+  title: "Купить подписку EA PLAY на PlayStation 2024",
+  description: "Быстро и безопасно приобретите подписку EA PLAY для своего аккаунта PlayStation. Новые возможности ждут вас!",
+  openGraph: {
+    title: "Купить подписку EA PLAY на PlayStation 2024",
+    description: "Быстро и безопасно приобретите подписку EA PLAY для своего аккаунта PlayStation. Новые возможности ждут вас!",
+    url: "/playstation_ea_play",
+    images: ["/og_images_catalogue/ps_ea_play.png"],
+    type: "website",
+  },
+  alternates: {
+    canonical: "/playstation_ea_play",
+  },
+};
+
+const checkParams = (slug: string | string[] | undefined) => {
+  if (slug && slug[0]) {
+    const num = parseInt(slug[0], 10);
+    if (!isNaN(num) && Number.isInteger(num) && (num === 1 || num === 12)) {
+      return slug[1];
+    }
+  }
+
+  return undefined;
+};
+
+export default function PsEaPlay({ params }: { params: { slug: string | string[] | undefined } }) {
+  return (
+    <div className="flex flex-col">
+      <Breadcrumbs>
+        <BreadcrumbItem>PlayStation EA Play</BreadcrumbItem>
+      </Breadcrumbs>
+
+      <ProductHero
+        icon={{ src: "/catalogue_icons/playstation_ea_play.jpg", alt: "PlayStation EA Play баннер" }}
+        title="Купить подписку EA Play для PlayStation в России"
+        description="EA Play дает доступ к эксклюзивным ежемесячным наградам, коллекции лучших игр EA, пробным версиям новинок EA и многому другому"
+        tags={[
+          {
+            icon: (
+              <div className="h-6 w-6 relative rounded-lg overflow-hidden">
+                <Image className="object-contain" src="/tr.png" alt="Регион турция" fill />
+              </div>
+            ),
+            text: "ТУРЦИЯ",
+          },
+          {
+            icon: <FlameIcon className="text-[#f95721]" />,
+            text: "ВЫГОДНО",
+          },
+          {
+            icon: <StarFillIcon className="text-primary" />,
+            text: "НЕ НУЖЕН VPN",
+          },
+        ]}
+      />
+
+      <FormComponent receivedDuration={checkParams(params.slug)} />
+
+      <AlsoRecommendToBuy
+        services={[
+          {
+            name: "PlayStation Аккаунт",
+            value: "playstation_account",
+            imageSrc: "/catalogue_icons/playstation_account.jpg",
+            alt: "PlayStation Аккаунт баннер",
+          },
+          {
+            name: "PlayStation Пополнение",
+            value: "playstation",
+            imageSrc: "/catalogue_icons/playstation_top_up.jpg",
+            alt: "PlayStation Пополнение баннер",
+          },
+          {
+            name: "PlayStation Plus",
+            value: "playstation_plus",
+            imageSrc: "/catalogue_icons/playstation_plus.jpg",
+            alt: "PlayStation Plus баннер",
+          },
+
+          {
+            name: "",
+            value: "",
+            imageSrc: "",
+            alt: "",
+          },
+        ]}
+      />
+
+      <Reviews />
+
+      <Faq>
+        <Question title="Как это работает?">
+          <p>
+            После успешной оплаты вам на почту придет письмо с ссылкой на активацию пополнения
+            <br />
+            <br />
+            На странице активации вам необходимо будет указать данные своего аккаунта PlayStation, после чего отправить нам заявку на
+            активацию
+            <br />
+            <br />
+            Наш менеджер получит вашу заявку и приобретет подписку EA Play на ваш аккаунт PlayStation
+          </p>
+        </Question>
+
+        <Question title="Как быстро я получу подписку EA Play на свой аккаунт PlayStation?">
+          <p>После отправки заявки среднее время выполнения заказа 5-30 минут в рабочие часы</p>
+        </Question>
+      </Faq>
+    </div>
+  );
+}
