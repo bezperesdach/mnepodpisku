169e0bdc4e83f3dd99e1020f175f67682aa17b4e
maslomeister <maslomeister@gmail.com>
2024-03-27 11:42:20 +0300

    reworked design

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,17 +1,24 @@
+import { Footer } from "@/components/footer";
+import { Button } from "@/components/ui/button";
+import { MainNavbar } from "@/components/ui/main-navbar";
 import Link from "next/link";
 
 export default function NotFound() {
   return (
     <div className="h-[100svh]">
-      {/* <MainNavbarisNotFound /> */}
-      <main className="h-[calc(100%-260px)] flex flex-col justify-center items-center">
-        <h1 className="text-3xl font-bold">404</h1>
-        <h2 className="text-xl font-semibold">Страница не найдена</h2>
-        <Link className="btn btn-secondary text-white mt-4" href="/">
-          На главную
-        </Link>
+      <MainNavbar />
+      <main className="min-h-[calc(100vh-334px)] flex flex-col justify-center items-center">
+        <div className="flex flex-col gap-2 justify-center rounded-lg bg-[#0c1430] p-4">
+          <h1 className="text-4xl font-bold">404</h1>
+          <h2 className="text-xl font-semibold">Страница не найдена</h2>
+          <Button asChild>
+            <Link className="mt-4" href="/">
+              На главную
+            </Link>
+          </Button>
+        </div>
       </main>
-      {/* <Footer /> */}
+      <Footer />
     </div>
   );
 }
