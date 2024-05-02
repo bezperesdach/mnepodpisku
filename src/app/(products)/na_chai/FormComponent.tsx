8ee4deba758b000e8e73b7724435fbd931775e3f
maslomeister <maslomeister@gmail.com>
2024-05-02 21:03:08 +0300

    redisgn

--- src/app/(products)/na_chai/FormComponent.tsx
+++ src/app/(products)/na_chai/FormComponent.tsx
@@ -1,17 +1,16 @@
 "use client";
 
 import { AppContext } from "@/components/AppContextWrapper/AppContextWrapper";
-import PaymentOptions from "@/components/PaymentOptions/PaymentOptions";
-import PriceComponent from "@/components/PriceComponent.tsx/PriceComponent";
-import TextInput from "@/components/TextInput/TextInput";
 import { getDonationPaymentLink } from "@/serverActions/createPaymentUrls";
 import { cn } from "@/lib/utils";
-import { ym } from "@/utils/ym";
-import { HashIcon, LockIcon } from "@primer/octicons-react";
+import { HashIcon, SyncIcon } from "@primer/octicons-react";
 import { useFormik } from "formik";
-import { usePathname, useRouter } from "next/navigation";
-import { useContext, useEffect, useState } from "react";
+import { useContext } from "react";
 import * as Yup from "yup";
+import TextInputV2 from "@/components/TextInput/TextInput";
+import { Button } from "@/components/ui/button";
+import RedirectingToPayment from "@/components/RedirectingToPayment/RedirectingToPayment";
+import { ym } from "@/utils/ym";
 
 type Props = {
   receivedAmount?: string;
@@ -25,20 +24,13 @@ const TopUpSchema = Yup.object().shape({
 });
 
 export default function FormComponent({ receivedAmount, ip }: Props) {
-  const pathname = usePathname();
-  const router = useRouter();
-
   const { dispatch } = useContext(AppContext);
 
-  const [loading, setLoading] = useState(false);
-
   const formik = useFormik({
-    initialValues: { amount: receivedAmount ?? "" },
+    initialValues: { amount: receivedAmount ?? "100" },
     validationSchema: TopUpSchema,
     onSubmit: async (values) => {
       formik.setSubmitting(true);
-      ym("reachGoal", "naChaiFund");
-      ym("reachGoal", "formaoplatit");
 
       const res = await getDonationPaymentLink(Number(values.amount), ip);
       dispatch({ type: "change_payment_link", payload: res.data.paymentUrl });
@@ -47,109 +39,205 @@ export default function FormComponent({ receivedAmount, ip }: Props) {
     validateOnBlur: true,
   });
 
-  useEffect(() => {
-    // const updatePrices = async (value: number) => {
-    //   const current = new URLSearchParams();
-    //   current.set("amount", value.toString());
-    //   const search = current.toString();
-    //   const query = search ? `?${search}` : "";
-    //   router.replace(`${pathname}${query}`, { scroll: false });
-
-    //   const updatedPrices = await getPsnBalancePrice(value);
-    //   setCalculatedAmount(updatedPrices.calculated);
-    //   setValue(updatedPrices.sale);
-    //   setLoading(false);
-    //   // console.log("updated");
-    // };
-
-    setLoading(true);
-
-    const value = formik.values.amount;
-    const error = formik.errors.amount;
-
-    if (value && !error) {
-      const current = new URLSearchParams();
-      current.set("amount", value.toString());
-      const search = current.toString();
-      const query = search ? `?${search}` : "";
-      router.replace(`${pathname}${query}`, { scroll: false });
-      setLoading(false);
-      // if (Number(value) >= 100) {
-      //   updatePrices(Number(value));
-      // }
-    } else {
-      setLoading(false);
-      // setCalculatedAmount(undefined);
-      // setValue(undefined);
-    }
-
-    if (value && error) {
-      setLoading(false);
-      // setCalculatedAmount(undefined);
-      // setValue(undefined);
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [formik.values.amount, formik.errors.amount]);
-
   return (
     <>
       <form onSubmit={formik.handleSubmit}>
-        <div className="flex flex-col md:flex-row mt-4 md:mt-14 gap-4 sm:gap-8 md:gap-16">
-          <div className="flex flex-col gap-1 lg:gap-6 w-full md:w-1/2">
-            <div className="flex flex-col gap-2 mb-2 md:mb-4 lg:mb-0">
-              <p className="label font-medium">Сумма отправления на чай и печеньки:</p>
-              <TextInput
-                icon={<HashIcon className="text-secondary" />}
-                type="number"
-                inputMode="numeric"
-                pattern="[0-9]*"
-                placeholder="Сумма пожертвования в рублях"
-                error={formik.errors.amount}
-                {...formik.getFieldProps("amount")}
-              />
-            </div>
+        <div className="w-full flex justify-center items-center mt-6">
+          <div className="w-full flex flex-col gap-2 max-w-screen-lg mx-2 p-6 rounded-3xl bg-[#0c1430]">
+            <p className="text-2xl md:text-3xl font-semibold tracking-tight">Укажите сумму пожертвования</p>
 
-            <div className="w-full flex-col gap-1 items-center hidden mt-4 md:flex lg:mt-0">
-              <button
-                type="submit"
-                className={cn("btn btn-secondary w-full text-white items-center", { "btn-disabled": formik.isSubmitting })}
-              >
-                {formik.isSubmitting ? (
-                  <span className="loading loading-spinner loading-xl flex-shrink-0" />
-                ) : (
-                  <LockIcon className="text-white text-xl" />
-                )}
-                Пожертвовать
-              </button>
-              <p className="text-center text-gray-500">После нажатия вы будете перенаправлены на страницу оплаты </p>
-            </div>
-          </div>
-          <div className="flex flex-col w-full md:w-1/2">
-            <PaymentOptions />
-            <PriceComponent
-              loading={loading}
-              value={undefined}
-              sale={
-                formik.values.amount !== "" ? (Number(formik.values.amount) > 0 ? Number(formik.values.amount) : undefined) : undefined
-              }
+            <TextInputV2
+              className="mt-4"
+              icon={<HashIcon className="text-inherit" />}
+              type="number"
+              label="Сумма пополнения в рублях"
+              inputMode="numeric"
+              pattern="[0-9]*"
+              placeholder="От 10 рублей"
+              error={formik.errors.amount}
+              {...formik.getFieldProps("amount")}
             />
-            <div className="w-full flex-col gap-1 items-center md:hidden mt-4 ">
-              <button
-                type="submit"
-                className={cn("btn btn-secondary w-full text-white items-center", { "btn-disabled": formik.isSubmitting })}
-              >
-                {formik.isSubmitting ? (
-                  <span className="loading loading-spinner loading-xl flex-shrink-0" />
-                ) : (
-                  <LockIcon className="text-white text-xl" />
-                )}
-                Пожертвовать
-              </button>
-              <p className="text-center text-gray-500">После нажатия вы будете перенаправлены на страницу оплаты </p>
-            </div>
+
+            <Button
+              className={cn("sticky bottom-0 mt-6 text-lg h-12", {
+                "bg-accent/50 text-muted-foreground": !formik.values.amount,
+                "pointer-events-none": formik.isSubmitting,
+              })}
+            >
+              {!formik.isSubmitting && formik.values.amount ? (
+                <p>
+                  ПОЖЕРТВОВАТЬ{" "}
+                  {formik.values.amount && <span className="text-[#fee525] text-xl font-bold">{formik.values.amount}₽</span>}
+                </p>
+              ) : (
+                <SyncIcon className="animate-spin" />
+              )}
+            </Button>
           </div>
         </div>
       </form>
+      <RedirectingToPayment
+        onRedirect={() => {
+          ym("reachGoal", "naChaiFund");
+          ym("reachGoal", "formaoplatit");
+        }}
+      />
     </>
   );
 }
+
+// "use client";
+
+// import { AppContext } from "@/components/AppContextWrapper/AppContextWrapper";
+// import PaymentOptions from "@/components/PaymentOptions/PaymentOptions";
+// import PriceComponent from "@/components/PriceComponent.tsx/PriceComponent";
+// import TextInput from "@/components/TextInput/TextInput";
+// import { getDonationPaymentLink } from "@/serverActions/createPaymentUrls";
+// import { cn } from "@/lib/utils";
+// import { ym } from "@/utils/ym";
+// import { HashIcon, LockIcon } from "@primer/octicons-react";
+// import { useFormik } from "formik";
+// import { usePathname, useRouter } from "next/navigation";
+// import { useContext, useEffect, useState } from "react";
+// import * as Yup from "yup";
+
+// type Props = {
+//   receivedAmount?: string;
+//   ip: string | null;
+// };
+
+// const TopUpSchema = Yup.object().shape({
+//   amount: Yup.number()
+//     .required("Необходимо заполнить")
+//     .test("Сумма больше 10", "Не может быть меньше 10", (value) => value >= 10),
+// });
+
+// export default function FormComponent({ receivedAmount, ip }: Props) {
+//   const pathname = usePathname();
+//   const router = useRouter();
+
+//   const { dispatch } = useContext(AppContext);
+
+//   const [loading, setLoading] = useState(false);
+
+//   const formik = useFormik({
+//     initialValues: { amount: receivedAmount ?? "" },
+//     validationSchema: TopUpSchema,
+//     onSubmit: async (values) => {
+//       formik.setSubmitting(true);
+//       ym("reachGoal", "naChaiFund");
+//       ym("reachGoal", "formaoplatit");
+
+//       const res = await getDonationPaymentLink(Number(values.amount), ip);
+//       dispatch({ type: "change_payment_link", payload: res.data.paymentUrl });
+//       formik.setSubmitting(false);
+//     },
+//     validateOnBlur: true,
+//   });
+
+//   useEffect(() => {
+//     // const updatePrices = async (value: number) => {
+//     //   const current = new URLSearchParams();
+//     //   current.set("amount", value.toString());
+//     //   const search = current.toString();
+//     //   const query = search ? `?${search}` : "";
+//     //   router.replace(`${pathname}${query}`, { scroll: false });
+
+//     //   const updatedPrices = await getPsnBalancePrice(value);
+//     //   setCalculatedAmount(updatedPrices.calculated);
+//     //   setValue(updatedPrices.sale);
+//     //   setLoading(false);
+//     //   // console.log("updated");
+//     // };
+
+//     setLoading(true);
+
+//     const value = formik.values.amount;
+//     const error = formik.errors.amount;
+
+//     if (value && !error) {
+//       const current = new URLSearchParams();
+//       current.set("amount", value.toString());
+//       const search = current.toString();
+//       const query = search ? `?${search}` : "";
+//       router.replace(`${pathname}${query}`, { scroll: false });
+//       setLoading(false);
+//       // if (Number(value) >= 100) {
+//       //   updatePrices(Number(value));
+//       // }
+//     } else {
+//       setLoading(false);
+//       // setCalculatedAmount(undefined);
+//       // setValue(undefined);
+//     }
+
+//     if (value && error) {
+//       setLoading(false);
+//       // setCalculatedAmount(undefined);
+//       // setValue(undefined);
+//     }
+//     // eslint-disable-next-line react-hooks/exhaustive-deps
+//   }, [formik.values.amount, formik.errors.amount]);
+
+//   return (
+//     <>
+//       <form onSubmit={formik.handleSubmit}>
+//         <div className="flex flex-col md:flex-row mt-4 md:mt-14 gap-4 sm:gap-8 md:gap-16">
+//           <div className="flex flex-col gap-1 lg:gap-6 w-full md:w-1/2">
+//             <div className="flex flex-col gap-2 mb-2 md:mb-4 lg:mb-0">
+//               <p className="label font-medium">Сумма отправления на чай и печеньки:</p>
+//               <TextInput
+//                 icon={<HashIcon className="text-secondary" />}
+//                 type="number"
+//                 inputMode="numeric"
+//                 pattern="[0-9]*"
+//                 placeholder="Сумма пожертвования в рублях"
+//                 error={formik.errors.amount}
+//                 {...formik.getFieldProps("amount")}
+//               />
+//             </div>
+
+//             <div className="w-full flex-col gap-1 items-center hidden mt-4 md:flex lg:mt-0">
+//               <button
+//                 type="submit"
+//                 className={cn("btn btn-secondary w-full text-white items-center", { "btn-disabled": formik.isSubmitting })}
+//               >
+//                 {formik.isSubmitting ? (
+//                   <span className="loading loading-spinner loading-xl flex-shrink-0" />
+//                 ) : (
+//                   <LockIcon className="text-white text-xl" />
+//                 )}
+//                 Пожертвовать
+//               </button>
+//               <p className="text-center text-gray-500">После нажатия вы будете перенаправлены на страницу оплаты </p>
+//             </div>
+//           </div>
+//           <div className="flex flex-col w-full md:w-1/2">
+//             <PaymentOptions />
+//             <PriceComponent
+//               loading={loading}
+//               value={undefined}
+//               sale={
+//                 formik.values.amount !== "" ? (Number(formik.values.amount) > 0 ? Number(formik.values.amount) : undefined) : undefined
+//               }
+//             />
+//             <div className="w-full flex-col gap-1 items-center md:hidden mt-4 ">
+//               <button
+//                 type="submit"
+//                 className={cn("btn btn-secondary w-full text-white items-center", { "btn-disabled": formik.isSubmitting })}
+//               >
+//                 {formik.isSubmitting ? (
+//                   <span className="loading loading-spinner loading-xl flex-shrink-0" />
+//                 ) : (
+//                   <LockIcon className="text-white text-xl" />
+//                 )}
+//                 Пожертвовать
+//               </button>
+//               <p className="text-center text-gray-500">После нажатия вы будете перенаправлены на страницу оплаты </p>
+//             </div>
+//           </div>
+//         </div>
+//       </form>
+//     </>
+//   );
+// }
--- src/app/(products)/na_chai/page.tsx
+++ src/app/(products)/na_chai/page.tsx
@@ -1,16 +1,19 @@
 import React from "react";
 import FormComponent from "./FormComponent";
 import { Metadata } from "next";
-import { isAmountValid } from "@/utils/utils";
 import Reviews from "@/components/Reviews/review";
 import { headers } from "next/headers";
+import { Breadcrumbs } from "@/components/breadcrumbs";
+import { BreadcrumbItem } from "@/components/ui/breadcrumb";
+import { ProductHero } from "@/components/product-hero";
+import { getAmountFromSlug } from "@/utils/utils";
 
 export const metadata: Metadata = {
-  title: "На чай и печеньки",
-  description: "Страница приема безвозмездных пожертвований, помоги нам приобрести чай и печеньки и сделать наш сервис еще лучше :3",
+  title: "Благодтворительность",
+  description: "Страница приема безвозмездных пожертвований, помоги нам приобрести чай и печеньки и сделать наш сервис еще лучше",
   openGraph: {
-    title: "На чай и печеньки",
-    description: "Страница приема безвозмездных пожертвований, помоги нам приобрести чай и печеньки и сделать наш сервис еще лучше :3",
+    title: "Благодтворительность",
+    description: "Страница приема безвозмездных пожертвований, помоги нам приобрести чай и печеньки и сделать наш сервис еще лучше",
     url: "/na_chai",
     images: ["/og_images_catalogue/na_chai.png"],
     type: "website",
@@ -20,36 +23,89 @@ export const metadata: Metadata = {
   },
 };
 
-type Props = {
-  searchParams: { [key: string]: string | string[] | undefined };
-};
-
-export default function PsEaPlay({ searchParams }: Props) {
+export default function PsEaPlay({ params }: { params: { slug: string } }) {
   const ip = headers().get("x-forwarded-for");
   return (
-    <div className="flex flex-col w-full max-w-screen-lg mx-auto mt-8 sm:mt-10 px-2 sm:px-4 mb-8">
-      <div className="flex flex-col min-[1240px]:flex-row items-center gap-4">
-        <h1 className="text-3xl font-semibold tracking-tight" id="heading">
-          Пожертвуй деньги на чай и печеньки
-        </h1>
-      </div>
+    <div className="flex flex-col">
+      <Breadcrumbs>
+        <BreadcrumbItem>Благодтворительность</BreadcrumbItem>
+      </Breadcrumbs>
 
-      <FormComponent receivedAmount={isAmountValid(searchParams["amount"])} ip={ip} />
+      <ProductHero
+        icon={{ src: "/catalogue_icons/playstation_top_up.jpg", alt: "Благодтворительность" }}
+        title="Пожертвование на развитие и улучшение проекта"
+        description="Воспользовавшись данной услугой вы можете пожертвовать любую сумму, которая пойдет на развитие и улучшение нашего проекта"
+        tags={[]}
+      />
 
-      <div className="mt-10">
-        <h2 className="text-xl lg:text-2xl font-bold" id="description">
-          Описание
-        </h2>
-        <p className="text-lg font-medium mt-4">
-          Если вам понравилось как нами была активирована ваша услуга или вы хотите нас похвалить, приобретение данной услуги это
-          отличная возможность это сделать!
-          <br />
-          <br />
-          Данные пожертвования помогут нам стать еще лучше и иногда приобретать себе чай с печеньем :3
-        </p>
-      </div>
+      <FormComponent receivedAmount={getAmountFromSlug(params.slug)} ip={ip} />
 
       <Reviews />
+
+      {/* <Faq>
+        <Question title="Как это работает?">
+          <p>
+            После успешной оплаты вам на почту придет письмо с ссылкой на активацию пополнения
+            <br />
+            <br />
+            На странице активации вам необходимо будет указать данные своего аккаунта PlayStation, после чего отправить нам заявку на
+            активацию
+            <br />
+            <br />
+            Наш менеджер получит вашу заявку и пополнит ваш аккаунт{" "}
+          </p>
+        </Question>
+
+        <Question title="Как быстро я получу деньги на свой аккаунт?">
+          <p>После отправки заявки среднее время выполнения заказа 5-30 минут в рабочие часы</p>
+        </Question>
+
+        <Question title="У меня нет турецкого аккаунта PlayStation">
+          <p>
+            Вы можете воспользоваться нашей услугой по{" "}
+            <a className="link text-primary" href="/playstation_account" target="_blank" rel="noopener noreferrer">
+              созданию аккаунта
+            </a>{" "}
+            или создать себе аккаунт самостоятельно по нашей{" "}
+            <a className="link text-primary" href="/blog/kak_samomu_sozdat_tureckiy_akaunt" target="_blank" rel="noopener noreferrer">
+              инструкции
+            </a>
+          </p>
+        </Question>
+      </Faq> */}
+
+      {/* <InstructionsSection>
+      <Instruction name="Как включить 2FA на аккаунте PSN?" file="kak_vkluchit_2fa_na_akaunte_psn" />
+      <Instruction name="Где найти резервные коды аккаунта PSN?" file="gde_posmotret_rezervnyi_kod" />
+      <Instruction name="Как изменить почту на PSN аккаунте" file="psn/how-to-change-email-psn.pdf" />
+      <Instruction name="Как создать турецкий аккаунт PSN" file="psn/how-to-create-turkish-psn.pdf" />
+      <Instruction name="Как добавить нового пользователя на PS4?" file="kak_dobavit_novogo_polzovatelya_na_PS4" />
+      <Instruction name="Как добавить нового пользователя на PS5?" file="kak_dobavit_novogo_polzovatelya_na_PS5" />
+    </InstructionsSection> */}
     </div>
+    // <div className="flex flex-col w-full max-w-screen-lg mx-auto mt-8 sm:mt-10 px-2 sm:px-4 mb-8">
+    //   <div className="flex flex-col min-[1240px]:flex-row items-center gap-4">
+    //     <h1 className="text-3xl font-semibold tracking-tight" id="heading">
+    //       Пожертвуй деньги на чай и печеньки
+    //     </h1>
+    //   </div>
+
+    //   <FormComponent receivedAmount={isAmountValid(searchParams["amount"])} ip={ip} />
+
+    //   <div className="mt-10">
+    //     <h2 className="text-xl lg:text-2xl font-bold" id="description">
+    //       Описание
+    //     </h2>
+    //     <p className="text-lg font-medium mt-4">
+    //       Если вам понравилось как нами была активирована ваша услуга или вы хотите нас похвалить, приобретение данной услуги это
+    //       отличная возможность это сделать!
+    //       <br />
+    //       <br />
+    //       Данные пожертвования помогут нам стать еще лучше и иногда приобретать себе чай с печеньем :3
+    //     </p>
+    //   </div>
+
+    //   <Reviews />
+    // </div>
   );
 }
