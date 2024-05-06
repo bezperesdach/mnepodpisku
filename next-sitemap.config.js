7c4b96d5d8ac2b5b96cf103e03f1a189da26b9cf
maslomeister <maslomeister@gmail.com>
2024-05-06 03:05:38 +0300

    excluded guides and cf email protection pages

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -4,7 +4,7 @@ module.exports = {
 	changefreq: 'daily',
 	priority: 0.7,
 	generateRobotsTxt: true,
-	exclude: ['/api/*', '/get_discount/*', '/get_discount', '/rbx', '/wb/*', '/wb', '/activate', '/activate/*', '/payment', '/payment/*'],
+	exclude: ['/api/*', '/get_discount/*', '/get_discount', '/rbx', '/wb/*', '/wb', '/activate', '/activate/*', '/payment', '/payment/*', '/guides', '/guides/*'],
 	additionalPaths: async (config) => [
 		await config.transform(config, '/adobe_creative_cloud'),
 		await config.transform(config, '/discord'),
@@ -25,12 +25,12 @@ module.exports = {
 				userAgent: '*',
 				allow: ['/playstation$', '/playstation_plus$', '/spotify$', '/playstation_account$', '/adobe_creative_cloud$', '/discord$'
 					, '/youtube$', '/netflix$', '/tinder$', '/xbox_game_pass$'],
-				disallow: ['/payment/verification', '/wb', '/digi', '/activate', '/get_discount', '/rbx', '/playstation', '/playstation_plus', '/spotify', '/playstation_account', '/adobe_creative_cloud', '/discord'
+				disallow: ['/cdn-cgi/l/email-protection', '/payment/verification', '/wb', '/digi', '/activate', '/get_discount', '/rbx', '/playstation', '/playstation_plus', '/spotify', '/playstation_account', '/adobe_creative_cloud', '/discord'
 					, '/youtube', '/netflix', '/tinder', '/xbox_game_pass', '/gde_posmotret_rezervnyi_kod',
 					'/kak_otpravit_chek_wb',
 					'/kak_otrpavit_soobshenie_prodavcu_wb',
 					'/kak_privyazat_kartu_k_turezkomu_akauntu_psn',
-					'/kak_vkluchit_2fa_na_akaunte_psn']
+					'/kak_vkluchit_2fa_na_akaunte_psn', '/guides']
 			},
 		],
 		transformRobotsTxt: async (_, robotsTxt) => `${robotsTxt}\n\nClean-param: amount&dur&sub&etext&_ym_debug`,
