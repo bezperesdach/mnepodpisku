e45decc088316ad14dd4d33dc791a8a48b0ce501
maslomeister <maslomeister@gmail.com>
2024-06-14 00:12:39 +0300

    custom error and not found

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,23 +1,41 @@
-"use client";
-
+"use client"; // Error components must be Client Components
 import { Footer } from "@/components/footer";
 import { Button } from "@/components/ui/button";
 import { MainNavbar } from "@/components/ui/main-navbar";
 import Link from "next/link";
+import Image from "next/image";
+import { useEffect } from "react";
 
-export default function NotFound() {
+export default function Error({ error, reset }: { error: Error & { digest?: string }; reset: () => void }) {
+  useEffect(() => {
+    // Log the error to an error reporting service
+    console.error(error);
+  }, [error]);
+
   return (
     <div className="h-[100svh]">
       <MainNavbar />
       <main className="min-h-[calc(100vh-334px)] flex flex-col justify-center items-center">
-        <div className="flex flex-col gap-2 justify-center rounded-lg bg-[#0c1430] p-4">
-          <h1 className="text-4xl font-bold">500</h1>
-          <h2 className="text-xl font-semibold">Что-то пошло не так</h2>
-          <Button asChild>
-            <Link className="mt-4" href="/">
-              На главную
-            </Link>
-          </Button>
+        <div className="flex flex-col items-center gap-4 mt-10">
+          <div className="aspect-square w-[250px] relative rounded-lg overflow-hidden">
+            <Image className="object-contain" src="/images/something_went_wrong.png" quality={98} alt="Страница не найдена" fill />
+          </div>
+          <div className="flex flex-col gap-2 justify-center rounded-lg bg-[#0c1430] p-4 ">
+            <h1 className="text-2xl font-semibold">Что-то пошло не так</h1>
+            <div className="flex flex-wrap gap-4 mt-4">
+              <Button
+                onClick={
+                  // Attempt to recover by trying to re-render the segment
+                  () => reset()
+                }
+              >
+                Попробовать снова
+              </Button>
+              <Button asChild>
+                <Link href="/">На главную</Link>
+              </Button>
+            </div>
+          </div>
         </div>
       </main>
       <Footer />
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,6 +1,7 @@
 import { Footer } from "@/components/footer";
 import { Button } from "@/components/ui/button";
 import { MainNavbar } from "@/components/ui/main-navbar";
+import Image from "next/image";
 import Link from "next/link";
 
 export default function NotFound() {
@@ -8,14 +9,18 @@ export default function NotFound() {
     <div className="h-[100svh]">
       <MainNavbar />
       <main className="min-h-[calc(100vh-334px)] flex flex-col justify-center items-center">
-        <div className="flex flex-col gap-2 justify-center rounded-lg bg-[#0c1430] p-4">
-          <h1 className="text-4xl font-bold">404</h1>
-          <h2 className="text-xl font-semibold">Страница не найдена</h2>
-          <Button asChild>
-            <Link className="mt-4" href="/">
-              На главную
-            </Link>
-          </Button>
+        <div className="flex flex-col items-center gap-4 mt-10">
+          <div className="aspect-square w-[250px] relative rounded-lg overflow-hidden">
+            <Image className="object-contain" src="/images/not_found.png" quality={98} alt="Страница не найдена" fill />
+          </div>
+          <div className="flex flex-col gap-2 justify-center rounded-lg bg-[#0c1430] p-4">
+            <h1 className="text-2xl font-semibold">Страница не найдена</h1>
+            <Button asChild>
+              <Link className="mt-4" href="/">
+                На главную
+              </Link>
+            </Button>
+          </div>
         </div>
       </main>
       <Footer />
