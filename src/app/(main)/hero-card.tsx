02925321d4d77e1814750d1ad1e1005f5dc1dcb1
Nikita Maslov <maslomeister@gmail.com>
2025-03-29 12:32:21 +0300

    feat: апрельский банер

--- src/app/(main)/hero-card.tsx
+++ src/app/(main)/hero-card.tsx
@@ -285,6 +285,21 @@ const PlayStation_Style = [
     },
     callToActionClass: "max-w-sm h-10 z-10 text-[#ffffff] bg-[#0070a0] hover:text-[#0070a0] hover:bg-[#ffffff] font-bold text-lg",
   },
+  // 19
+  {
+    image: "/images/Playstation_topup_2025_april.avif",
+    titleClass: "text-xl font-bold tracking-tight text-[#ffffff] bg-[#007de8] px-4 py-2 mr-auto md:text-4xl",
+    titleStyle: {
+      // textShadow: "-2px -2px 0 #000,2px -2px 0 #000,-2px 2px 0 #000,2px 2px 0 #000",
+    },
+    descriptionClass:
+      "text-sm font-bold leading-7 max-w-xs text-[#007de8] bg-[#ffffff] px-2 md:px-4 py-[1px] md:py-1 mr-auto md:text-base",
+    descriptionStyle: {
+      // textShadow: "-1px -1px 0 #000,1px -1px 0 #000,-1px 1px 0 #000,1px 1px 0 #000",
+    },
+    callToActionClass:
+      "max-w-sm h-10 z-10 text-[#ffffff] bg-[#007CE7] hover:bg-[#ffffff]  hover:text-[#007CE7] font-bold text-lg border-2 border-[#007CE7]",
+  },
 ];
 
 const PlayStation_Plus_Style = [
@@ -336,13 +351,13 @@ const Banners = [
     title: "ПОПОЛНЕНИЕ PLAYSTATION",
     description: "СКИДКИ ДО 75%",
     callToAction: "ПОПОЛНИТЬ",
-    image: PlayStation_Style[18].image,
+    image: PlayStation_Style[19].image,
     alt: "Playstation пополнение банер",
-    titleClass: PlayStation_Style[18].titleClass,
-    titleStyle: PlayStation_Style[18].titleStyle,
-    descriptionClass: PlayStation_Style[18].descriptionClass,
-    descriptionStyle: PlayStation_Style[18].descriptionStyle,
-    callToActionClass: PlayStation_Style[18].callToActionClass,
+    titleClass: PlayStation_Style[19].titleClass,
+    titleStyle: PlayStation_Style[19].titleStyle,
+    descriptionClass: PlayStation_Style[19].descriptionClass,
+    descriptionStyle: PlayStation_Style[19].descriptionStyle,
+    callToActionClass: PlayStation_Style[19].callToActionClass,
     href: "playstation",
   },
   {
